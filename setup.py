d3372364e595fcfddc2f9e1dfcc19f79d808b0d7
Kristoffer Sahlin <kxs624@psu.edu>
2018-01-04 23:33:28 -0500

    fixed indentation inconsistencies

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='mathstats',
-    version='0.2.6.1',
+    version='0.2.6.2',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
